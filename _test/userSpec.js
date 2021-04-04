e5524e0fc3042358c5bccefbfcb789e79c5f347a
Julia Rosehub <email@example.com>
2021-04-05 01:26:05 +0300

    fix css

--- _test/userSpec.js
+++ _test/userSpec.js
@@ -1,9 +1,9 @@
-process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = "test";
 
-const User = require('../models/user'),
-  { expect } = require('chai');
+const User = require("../models/user"),
+  { expect } = require("chai");
 
-require('../main');
+require("../main");
 
 beforeEach(done => {
   User.deleteMany({})
@@ -12,16 +12,16 @@ beforeEach(done => {
     });
 });
 
-describe('SAVE user', () => {
-  it('should save one user without error', (done) => {
-    const testUser = new User({
+describe("SAVE user", () => {
+  it("should save user without error", (done) => {
+    let testUser = new User({
       name: {
-        first: 'John',
-        last: 'Snow'
+        first: "John",
+        last: "Snow",
       },
-      email: 'john@example.com',
-      password: 'password',
-      zipCode: 200701,
+      email: "john@example.com",
+      password: 12345,
+      zipCode: 200208,
     });
     testUser.save()
       .then(() => {
@@ -30,21 +30,9 @@ describe('SAVE user', () => {
             expect(result.length)
               .to.eq(1);
             expect(result[0])
-              .to.have.property('_id');
+              .to.have.property("_id");
             done();
           });
-      });
-  });
-});
-
-describe("/users GET", () => {
-  it("it should GET all the users", (done) => {
-    chai.request(app)
-      .get("/users")
-      .end((errors, res) => {
-        expect(res).to.have.status(200);
-        expect(errors).to.be.equal(null);
-        done();
       });
   });
 });
--- _test/usersControllerSpec.js
+++ _test/usersControllerSpec.js
@@ -1,31 +1,30 @@
-const chai = require('chai');
-const { expect } = chai;
+const chai = require("chai"),
+  { expect } = chai,
+  usersController = require("../controllers/usersController");
 
-const usersController = require('../controllers/usersController');
-
-describe('usersController', () => {
-  describe('getUsersParams', () => {
-    it('should convert request body to contain the name attributes of the user object', () => {
+describe("usersController", () => {
+  describe("getUserParams", () => {
+    it("should convert request body to contain the name attributes of the user object", () => {
       const body = {
         first: "John",
         last: "Snow",
         email: "john@example.com",
         password: 12345,
-        zipCode: 200700,
+        zipCode: 200205,
       };
       expect(usersController.getUserParams(body))
         .to.deep.include({
           name: {
-            first: 'John',
-            last: 'Snow'
+            first: "John",
+            last: "Snow",
           }
         });
     });
-  });
 
-  it("should return an empty object with empty request body input", () => {
-    const emptyBody = {};
-    expect(usersController.getUserParams(emptyBody))
-      .to.deep.include({});
+    it("should return an empty object if request body is empty", () => {
+      const emptyBody = {};
+      expect(usersController.getUserParams(emptyBody))
+        .to.deep.include({});
+    });
   });
 });
